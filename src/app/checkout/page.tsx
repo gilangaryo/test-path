ea9f307b61d958a7c1ba14a9ee4c4e27166e2bfa
Gilang Aryo <gilang91668@gmail.com>
2025-07-07 22:06:56 +0700

    fixit

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -2,7 +2,7 @@
 
 import { loadStripe } from '@stripe/stripe-js';
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+const stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY!);
 
 export default function CheckoutPage() {
     const handleCheckout = async () => {
